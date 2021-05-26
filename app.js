2a99f4fcffffd1ad66b69b56e27be214bb5daf4a
Fayyaz Alam <fayazalam.98@gmail.com>
2021-05-26 14:25:14 -0700

    questions added

--- app.js
+++ app.js
@@ -21,3 +21,28 @@
 // var number = +prompt("Enter NUmber")
 //  num = Math.abs(number);
 //  document.write("Absolute value "+num +" <br/> User Input "+number)
+
+//Q4
+
+// var round = Math.ceil(Math.random() * 6)
+// document.write("<h3>THE DICE GAME</h3> <br/> <h2>Random Dice Number is:   "+round+"</h2>")
+
+//Q5
+// var player1 =prompt("Enter first player name") 
+// var player2 =prompt("Enter second player name")
+// var  dise = prompt("player 1 Enter heads or tail")
+// var randomdise;
+// var randomNum = Math.round(Math.random())
+// console.log(randomNum)
+// if (randomNum === 1) { 
+// randomdise="heads"  }
+// else if ( randomNum === 0){
+// randomdise="tails"   }
+
+// document.write("flipcoin"+"<div> Player One "+player1+"</div>"+"<div> Player two "+player2+"</div>")
+// if(dise.toLowerCase === randomdise){
+//     document.write(player1+" Won")
+// }
+// else{
+//     document.write(player1+" Won")
+// }
