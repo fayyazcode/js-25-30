28652ac0b00128c97959459a61caa92ed3716af8
Fayyaz Alam <fayazalam.98@gmail.com>
2021-05-26 13:48:53 -0700

    push code

---
+++ app.js
@@ -0,0 +1,23 @@
+//Q1
+// var number = prompt("Enter NUmber")
+// var round = Math.round(number)
+// var ceil = Math.ceil(number)
+// var floor = Math.floor(number)
+
+// document.write("<h2>Number: "+number+ "<br/> Round off value: "+ round+ "<br/>Ceil value: "+ceil+"<br/> Floor value: "+floor+"</h2>");
+
+
+//Q2
+
+// var number = prompt("Enter  negative NUmber")
+// var round = Math.round(number)
+// var ceil = Math.ceil(number)
+// var floor = Math.floor(number)
+
+// document.write("<h2>Number: "+number+ "<br/> Round off value: "+ round+ "<br/>Ceil value: "+ceil+"<br/> Floor value: "+floor+"</h2>");
+
+//Q3
+
+// var number = +prompt("Enter NUmber")
+//  num = Math.abs(number);
+//  document.write("Absolute value "+num +" <br/> User Input "+number)
